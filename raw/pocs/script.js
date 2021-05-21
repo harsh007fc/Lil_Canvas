11c816e57aacc85554a0eb6b64d4e69d5110f297
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-05-21 15:38:10 +0530

    code addded upto forever bouncing ball

--- raw/pocs/script.js
+++ raw/pocs/script.js
@@ -80,32 +80,80 @@ let cElem = canvas.getContext("2d");  //to instantialte canvas context
 // cElem.stroke();
 
 
-// now lets make a smiley
-let centerX = canvas.width/2;
-let centerY = canvas.height/2;
-cElem.lineWidth = 10;
-cElem.beginPath();
-cElem.fillStyle = "blue"
-cElem.arc(centerX,centerY,200,0,Math.PI * 2);
+// // now lets make a smiley
+// let centerX = canvas.width/2;
+// let centerY = canvas.height/2;
+// cElem.lineWidth = 10;
+// cElem.beginPath();
+// cElem.fillStyle = "blue"
+// cElem.arc(centerX,centerY,200,0,Math.PI * 2);
 
-// moto for mouth
-cElem.moveTo(centerX + 150,centerY);
+// // moto for mouth
+// cElem.moveTo(centerX + 150,centerY);
 
-// drawing mouth
-cElem.arc(centerX,centerY,150,0,Math.PI ,false);
-
-
-// move to left eye
-cElem.moveTo(centerX - 70,centerY - 100);
-
-// draw the left eye
-cElem.arc(centerX - 100,centerY - 100,30,0,Math.PI *2);
-
-// move to center
-cElem.moveTo(centerX + 130,centerY - 100);
+// // drawing mouth
+// cElem.arc(centerX,centerY,150,0,Math.PI ,false);
 
 
-cElem.arc(centerX + 100,centerY - 100,30,0,Math.PI *2);
+// // move to left eye
+// cElem.moveTo(centerX - 70,centerY - 100);
 
-cElem.stroke();
-// cElem.fill();
+// // draw the left eye
+// cElem.arc(centerX - 100,centerY - 100,30,0,Math.PI *2);
+
+// // move to center
+// cElem.moveTo(centerX + 130,centerY - 100);
+
+
+// cElem.arc(centerX + 100,centerY - 100,30,0,Math.PI *2);
+
+// cElem.stroke();
+// // cElem.fill();
+
+
+
+
+///ANIMATIONS
+//lets draw an animation
+
+let circle = {
+    x:200,
+    y:200,
+    size:30,
+    dx:6,
+    dy:5
+};
+
+function drawCircle(){
+    cElem.beginPath();
+    cElem.arc(circle.x,circle.y,circle.size,0,Math.PI * 2);
+    cElem.fillStyle = "grey";
+    cElem.fill();
+}
+
+
+function update(){
+
+    cElem.clearRect(0,0,canvas.width,canvas.height);
+    drawCircle();
+    //change position
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+
+
+    // detect side walls
+    if(circle.x + circle.size > canvas.width || circle.x - circle.size < 0){
+        circle.dx *= -1;
+    }
+
+    // detect top and bottom walls
+    if(circle.y + circle.size > canvas.width || circle.y - circle.size < 0){
+        circle.dy *= -1;
+    }
+
+
+    requestAnimationFrame(update);
+
+}
+
+update();
